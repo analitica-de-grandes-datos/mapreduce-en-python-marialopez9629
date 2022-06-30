d792e4e10b9381be8a354d2cd0ad4d12a395b38f
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2022-06-30 11:15:04 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,39 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+#! /usr/bin/python3
+import sys
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        letra= line.split("\n")[0]
+        
+        if curkey==letra:
+           total+=1
+        else:
+        
+           if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{},{}\n".format(curkey, total))
+                
+           curkey=letra
+           total=1
+    
+    #sorted_d = dict(sorted(curkey.items(), key=operator.itemgetter(1)))
+    
+    #for key,val in sorted_d.items():
+    sys.stdout.write("{},{}\n".format(curkey, total))
 #
