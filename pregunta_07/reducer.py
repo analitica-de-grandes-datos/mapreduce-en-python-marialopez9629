3671e4d5c5a8ba3b4e8b4b5ce0f36eefce1fc2e0
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2022-06-30 14:45:56 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,54 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+#
+#
+#
+#! /usr/bin/python3
+import sys
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+
+if __name__ == '__main__':
+
+    dicc={}
+    lista=[]
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        line=line.split("\n")[0]
+        letra,date,value=line.split(",")
+        dicc[letra,int(value)]=date
+    
+    sorted_d=dict(sorted(dicc.items()))
+    
+    for key,date in sorted_d.items():
+    	sys.stdout.write("{}\t{}\t{}\n".format(key[0], date,key[1]))
+    	
+    
+    #print(lista)
+      #  value=float(value)
+        
+       # if curkey==letra:
+        #   if value>maxi:
+         #     maxi=value
+         #  elif value<mini:
+         #     mini=value
+        #else:
+        
+         #  if curkey is not None:
+
+          #      sys.stdout.write("{}\t{}\t{}\n".format(curkey, maxi,mini))
+                
+          # curkey=letra
+          # mini=value
+          # maxi=value
+    
+   
+    #sys.stdout.write("{}\t{}\t{}\n".format(curkey, maxi,mini))
+#
+#
+#
 #
