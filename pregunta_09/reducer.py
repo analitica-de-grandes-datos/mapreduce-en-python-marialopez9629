81069251745a69d413c1db38a3041381a1e4bf30
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2022-06-30 12:19:43 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,30 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+#! /usr/bin/python3
+import sys
+import operator
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+
+if __name__ == '__main__':
+
+    curkey = None
+    suma=0
+    contador=0
+    dicc={}
+
+    for line in sys.stdin:
+
+        line=line.split("\n")[0]
+        letra,date,value=line.split(",")
+        value=int(value)
+        dicc[value]=[letra,date]
+        resultado=dict(sorted(dicc.items()))       
+
+    for key, value in resultado.items():
+        contador+=1
+        sys.stdout.write("{}\t{}\t{}\n".format(value[0],value[1],key))
+        if contador==6:
+           break
+                
 #
