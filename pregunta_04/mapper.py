109b4d662682b3466846d4a5d1c3a143684ee4b7
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2022-06-30 11:14:42 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,14 @@
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+#! /usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+        vector=line.split('\n')[0]
+        letra=vector[0]     
+        sys.stdout.write("{}\n".format(letra))
 #
