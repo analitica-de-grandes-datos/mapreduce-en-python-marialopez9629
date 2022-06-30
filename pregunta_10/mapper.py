1360ff78309eb60f112ff48aed408442b7339461
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2022-06-30 13:53:40 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+#! /usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        line=line.replace('\n','')
+        vector=line.split('\t')
+        clave=vector[0]
+        letras=vector[1]
+        sys.stdout.write("{}\t{}\n".format(clave,letras))
+#
 #
